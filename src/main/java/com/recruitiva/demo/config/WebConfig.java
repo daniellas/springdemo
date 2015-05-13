2daf2b36de35184c039339fd7b7a59c9c75553ed
Daniel Las <lasdaniel@gmail.com>
2015-05-13 14:59:32 +0200

    Stage 1

--- src/main/java/com/recruitiva/demo/config/WebConfig.java
+++ src/main/java/com/recruitiva/demo/config/WebConfig.java
@@ -1,21 +1,15 @@
 package com.recruitiva.demo.config;
 
-import java.util.List;
-
 import javax.persistence.EntityManagerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-
-import com.recruitiva.demo.HibernateAwareObjectMapper;
 
 @Configuration
 @EnableWebMvc
@@ -40,16 +34,4 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         return resolver;
     }
     
-    @Bean
-    public HibernateAwareObjectMapper hibernateAwareObjectMapper() {
-        return new HibernateAwareObjectMapper(emf);
-    }
-
-    @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-
-        converter.setObjectMapper(hibernateAwareObjectMapper());
-        converters.add(converter);
-    }
 }
