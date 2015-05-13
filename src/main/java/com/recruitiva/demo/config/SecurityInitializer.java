99659426107c4b62ee36897a8aa814ac304fbbeb
Daniel Las <lasdaniel@gmail.com>
2015-05-13 15:06:58 +0200

    Some

--- src/main/java/com/recruitiva/demo/config/SecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.recruitiva.demo.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
