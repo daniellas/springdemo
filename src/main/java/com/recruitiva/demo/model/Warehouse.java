9be2747ae9105d247d2f9fbca1e6f3e7266fa0e3
Daniel Las <lasdaniel@gmail.com>
2015-05-14 10:23:49 +0200

    Article search

--- src/main/java/com/recruitiva/demo/model/Warehouse.java
+++ src/main/java/com/recruitiva/demo/model/Warehouse.java
@@ -3,6 +3,8 @@ package com.recruitiva.demo.model;
 import java.util.List;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,4 +27,6 @@ public interface Warehouse {
 
     @Secured({ "ROLE_ADMIN" })
     Article saveArticle(@Valid Article article);
+
+    List<Article> search(@NotNull @Size(min = 2) String name);
 }
--- src/main/java/com/recruitiva/demo/repository/ArticleRepository.java
+++ src/main/java/com/recruitiva/demo/repository/ArticleRepository.java
@@ -3,10 +3,15 @@ package com.recruitiva.demo.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.recruitiva.demo.entity.Article;
 import com.recruitiva.demo.entity.Category;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
     List<Article> findByCategory(Category category);
+
+    @Query("select a from Article a where a.name like %:name%")
+    List<Article> search(@Param("name") String name);
 }
